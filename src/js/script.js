f5f02da0729caac912cf65ec5ca69e9e2466ad22
alexandragauss <alexandra_gauss@uri.edu>
2023-03-22 11:04:23 -0400

    Stop spin on smaller screens; disable zoom

--- src/js/script.js
+++ src/js/script.js
@@ -31,7 +31,7 @@
 			style: 'mapbox://styles/mapbox/satellite-v9', // choose a style: https://docs.mapbox.com/api/maps/#styles
 			center: [ 30, 20 ], // starting position [lng, lat]
 			zoom: 2.00, // starting zoom
-			maxZoom: 3,
+			maxZoom: 2,
 			minZoom: 2,
 			transformRequest,
 			projection: 'globe',
@@ -46,80 +46,82 @@
 			map.setFog( {} );
 		} );
 
-		//spin
+		//spin on larger screen sizes
+		if ( $(window).width() > 750) {
 		// The following values can be changed to control rotation speed:
 
-		// At low zooms, complete a revolution every two minutes.
-		const secondsPerRevolution = 120;
-		// Above zoom level 5, do not rotate.
-		const maxSpinZoom = 5;
-		// Rotate at intermediate speeds between zoom levels 3 and 5.
-		const slowSpinZoom = 3;
+			// At low zooms, complete a revolution every two minutes.
+			const secondsPerRevolution = 120;
+			// Above zoom level 5, do not rotate.
+			const maxSpinZoom = 5;
+			// Rotate at intermediate speeds between zoom levels 3 and 5.
+			const slowSpinZoom = 3;
 
-		let userInteracting = false;
-		let spinEnabled = true;
+			let userInteracting = false;
+			let spinEnabled = true;
 
-		function spinGlobe() {
-			const zoom = map.getZoom();
-			if ( spinEnabled && ! userInteracting && zoom < maxSpinZoom ) {
-				let distancePerSecond = 360 / secondsPerRevolution;
-				if ( zoom > slowSpinZoom ) {
+			function spinGlobe() {
+				const zoom = map.getZoom();
+				if ( spinEnabled && ! userInteracting && zoom < maxSpinZoom ) {
+					let distancePerSecond = 360 / secondsPerRevolution;
+					if ( zoom > slowSpinZoom ) {
 					// Slow spinning at higher zooms
-					const zoomDif =
+						const zoomDif =
 ( maxSpinZoom - zoom ) / ( maxSpinZoom - slowSpinZoom );
-					distancePerSecond *= zoomDif;
+						distancePerSecond *= zoomDif;
+					}
+					const center = map.getCenter();
+					center.lng -= distancePerSecond;
+					// Smoothly animate the map over one second.
+					// When this animation is complete, it calls a 'moveend' event.
+					map.easeTo( { center, duration: 1000, easing: ( n ) => n } );
 				}
-				const center = map.getCenter();
-				center.lng -= distancePerSecond;
-				// Smoothly animate the map over one second.
-				// When this animation is complete, it calls a 'moveend' event.
-				map.easeTo( { center, duration: 1000, easing: ( n ) => n } );
 			}
-		}
 
-		// Pause spinning on interaction
-		map.on( 'mousedown', () => {
-			userInteracting = true;
-		} );
+			// Pause spinning on interaction
+			map.on( 'mousedown', () => {
+				userInteracting = true;
+			} );
 
-		// Restart spinning the globe when interaction is complete
-		map.on( 'mouseup', () => {
-			userInteracting = false;
-			spinGlobe();
-		} );
-
-		// These events account for cases where the mouse has moved
-		// off the map, so 'mouseup' will not be fired.
-		map.on( 'dragend', () => {
-			userInteracting = false;
-			spinGlobe();
-		} );
-		map.on( 'pitchend', () => {
-			userInteracting = false;
-			spinGlobe();
-		} );
-		map.on( 'rotateend', () => {
-			userInteracting = false;
-			spinGlobe();
-		} );
-
-		// When animation is complete, start spinning if there is no ongoing interaction
-		map.on( 'moveend', () => {
-			spinGlobe();
-		} );
-
-		document.getElementById( 'btn-spin' ).addEventListener( 'click', ( e ) => {
-			spinEnabled = ! spinEnabled;
-			if ( spinEnabled ) {
+			// Restart spinning the globe when interaction is complete
+			map.on( 'mouseup', () => {
+				userInteracting = false;
 				spinGlobe();
-				e.target.innerHTML = 'Pause rotation';
-			} else {
-				map.stop(); // Immediately end ongoing animation
-				e.target.innerHTML = 'Start rotation';
-			}
-		} );
+			} );
 
-		spinGlobe();
+			// These events account for cases where the mouse has moved
+			// off the map, so 'mouseup' will not be fired.
+			map.on( 'dragend', () => {
+				userInteracting = false;
+				spinGlobe();
+			} );
+			map.on( 'pitchend', () => {
+				userInteracting = false;
+				spinGlobe();
+			} );
+			map.on( 'rotateend', () => {
+				userInteracting = false;
+				spinGlobe();
+			} );
+
+			// When animation is complete, start spinning if there is no ongoing interaction
+			map.on( 'moveend', () => {
+				spinGlobe();
+			} );
+
+			document.getElementById( 'btn-spin' ).addEventListener( 'click', ( e ) => {
+				spinEnabled = ! spinEnabled;
+				if ( spinEnabled ) {
+					spinGlobe();
+					e.target.innerHTML = 'Pause';
+				} else {
+					map.stop(); // Immediately end ongoing animation
+					e.target.innerHTML = 'Spin';
+				}
+			} );
+
+			spinGlobe();
+		}
 
 		//Map View Radio buttons
 		const viewList = document.getElementById( 'menu' );
@@ -142,7 +144,7 @@
 				dataType: 'text',
 				success( csvData ) {
 					makeGeoJSON( csvData );
-},
+				},
 			} );
 
 			function makeGeoJSON( csvData ) {
@@ -161,7 +163,7 @@
 								'data': data
 							},
 							'paint': {
-								'circle-radius': 12,
+								'circle-radius': 10,
 								'circle-color': '#c0ddf2'
 							}
 						} );
