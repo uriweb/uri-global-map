a356b9006c73c31903a8a5710f9905df31790ea9
alexandragauss <alexandra_gauss@uri.edu>
2023-12-08 12:15:41 -0500

    Slow rotation

--- src/js/script.js
+++ src/js/script.js
@@ -73,7 +73,7 @@
 			function spinGlobe() {
 				const zoom = map.getZoom();
 				if ( spinEnabled && ! userInteracting && zoom < maxSpinZoom ) {
-					let distancePerSecond = 360 / secondsPerRevolution;
+					let distancePerSecond = 200 / secondsPerRevolution;
 					if ( zoom > slowSpinZoom ) {
 					// Slow spinning at higher zooms
 						const zoomDif =
